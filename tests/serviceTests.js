d6646ff20357c685212887d9c3fe453ec1321017
Ian McKenney <student@epicodus.com>
2015-10-13 16:48:54 -0700

    remove  from service tests

--- tests/serviceTests.js
+++ tests/serviceTests.js
@@ -13,7 +13,6 @@ describe('CoursesFactory test', function() {
 
   it('adds a course using the method .addCourse()', function() {
     var course = { name: "PHP/JavaScript/Drupal", id: 1, students: [] };
-    var $scope = {};
     CoursesFactory.courseName = "PHP/JavaScript/Drupal";
     CoursesFactory.addCourse();
     expect(CoursesFactory.courses[0].name).toBe("PHP/JavaScript/Drupal");
